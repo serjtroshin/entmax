44dedf779fb659735ac75f0882f75292f58ee85e
tkachev andrej <andronikqwe55@gmail.com>
2019-12-03 15:57:15 +0300

    train on gpu

--- scripts/train_inflection_attention_15_gen_15_biscet.sh
+++ scripts/train_inflection_attention_15_gen_15_biscet.sh
@@ -3,6 +3,8 @@ cd OpenNMT-py
 python setup.py install
 dir="../"
 onmt_train -data $dir/data/demo \
+--gpu_ranks 0 \
+--world_size 1 \
 --enc_rnn_size 300 \
 --dec_rnn_size 300 \
 --src_word_vec_size 300 \
